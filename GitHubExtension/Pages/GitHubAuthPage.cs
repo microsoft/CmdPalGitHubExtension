9fc4ae47cbbaa61d0e633bd42ef353c0160904a1
Lauren Ciha <laurenciha@microsoft.com>
2025-01-31 14:19:38 -0800

    Loading animation plays while user is signing in

--- GitHubExtension/Pages/GitHubAuthPage.cs
+++ GitHubExtension/Pages/GitHubAuthPage.cs
@@ -12,13 +12,41 @@ namespace GitHubExtension.Pages;
 
 internal sealed partial class GitHubAuthPage : FormPage
 {
-    private readonly GitHubAuthForm _authForm = new();
+    private readonly GitHubAuthForm _authForm;
 
-    public override IForm[] Forms() => [_authForm];
+    public override IForm[] Forms() => new IForm[] { _authForm };
 
     internal event TypedEventHandler<object, SignInStatusChangedEventArgs>? SignInAction
     {
         add => _authForm.SignInAction += value;
         remove => _authForm.SignInAction -= value;
+    }
+
+    public GitHubAuthPage()
+    {
+        _authForm = new();
+        _authForm.SignInAction += OnSignInCompleted;
+        _authForm.LoadingStateChanged += OnLoadingChanged;
+    }
+
+    private void OnSignInCompleted(object sender, SignInStatusChangedEventArgs args)
+    {
+        if (args.Error != null)
+        {
+            IsLoading = false;
+            var message = new StatusMessage() { Message = $"Error in sign-in: {args.Error.Message}", State = MessageState.Error };
+            ExtensionHost.Host?.ShowStatus(message);
+        }
+        else if (args.IsSignedIn)
+        {
+            IsLoading = false;
+            var message = new StatusMessage() { Message = "Sign in succeeded!", State = MessageState.Success };
+            ExtensionHost.Host?.ShowStatus(message);
+        }
+    }
+
+    private void OnLoadingChanged(object sender, bool isLoading)
+    {
+        IsLoading = isLoading;
     }
 }
