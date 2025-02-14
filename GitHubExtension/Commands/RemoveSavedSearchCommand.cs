a39ac5a8cec0675198df8bf2b004864152e3d769
Lauren Ciha <laurenciha@microsoft.com>
2025-02-13 17:07:27 -0800

    [Draft] Add remove saved search command

---
+++ GitHubExtension/Commands/RemoveSavedSearchCommand.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using GitHubExtension.DataModel.DataObjects;
+using GitHubExtension.Helpers;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace GitHubExtension.Commands;
+
+public partial class RemoveSavedSearchCommand : InvokableCommand
+{
+    private readonly Search savedSearch;
+
+    public RemoveSavedSearchCommand(Search search)
+    {
+        savedSearch = search;
+        Name = "Remove";
+        Icon = new IconInfo("\uE8A7");
+    }
+
+    public override CommandResult Invoke()
+    {
+        SearchHelper.Instance.RemoveSavedSearch(savedSearch);
+
+        return CommandResult.KeepOpen();
+    }
+}
