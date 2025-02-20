a8766aff334a05cc040e3116493718095472ed64
guimafelipe <guima.felipec@gmail.com>
2025-02-20 10:55:20 -0800

    Changing remove to explicit SQL queries

--- GitHubExtension/Pages/SavedSearchesPage.cs
+++ GitHubExtension/Pages/SavedSearchesPage.cs
@@ -2,7 +2,6 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using GitHubExtension.DataModel.Enums;
 using GitHubExtension.Commands;
 using GitHubExtension.Forms;
 using GitHubExtension.Helpers;
--- GitHubExtension/PersistentData/Search.cs
+++ GitHubExtension/PersistentData/Search.cs
@@ -14,6 +14,10 @@ namespace GitHubExtension.PersistentData;
 [Table("Search")]
 public class Search
 {
+    private static readonly Lazy<ILogger> _logger = new(() => Log.ForContext("SourceContext", $"PersistentData/{nameof(Search)}"));
+
+    private static readonly ILogger _log = _logger.Value;
+
     [Key]
     public long Id { get; set; }
 
@@ -59,7 +63,14 @@ public class Search
 
     public static void Remove(DataStore datastore, string name, string searchString)
     {
-        datastore.Connection.Delete(new Search { Name = name, SearchString = searchString });
+        var sql = "DELETE FROM Search WHERE Name = @Name AND SearchString = @SearchString";
+        var command = datastore.Connection!.CreateCommand();
+        command.CommandText = sql;
+        command.Parameters.AddWithValue("@Name", name);
+        command.Parameters.AddWithValue("@SearchString", searchString);
+        _log.Verbose(DataStore.GetCommandLogMessage(sql, command));
+        var deleted = command.ExecuteNonQuery();
+        _log.Verbose(DataStore.GetDeletedLogMessage(deleted));
     }
 
     public static IEnumerable<Search> GetAll(DataStore datastore)
