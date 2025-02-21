2283f53bc0c9993dd1a445a8fa2882a5bab4fa23
guimafelipe <guima.felipec@gmail.com>
2025-02-21 10:19:20 -0800

    Some magic

---
+++ GitHubExtension/Pages/SearchPages/SearchPageFactory.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using GitHubExtension.DataModel.Enums;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace GitHubExtension;
+
+public class SearchPageFactory
+{
+    public static ListPage CreateForSearch(PersistentData.Search search)
+    {
+        return search.Type switch
+        {
+            SearchType.PullRequests => new PullRequestsSearchPage(search),
+            SearchType.Issues => new IssuesSearchPage(search),
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
