0b33663c6ef448903b349e9a63b29ddb8c7e14c2
Lauren Ciha <laurenciha@microsoft.com>
2025-03-15 19:02:48 -0400

    Search --> Query in SaveSearchTemplate

--- GitHubExtension/Controls/Forms/SaveSearchForm.cs
+++ GitHubExtension/Controls/Forms/SaveSearchForm.cs
@@ -34,6 +34,11 @@ public sealed partial class SaveSearchForm : FormContent, IGitHubForm
         { "{{SavedSearchString}}", _savedSearch.SearchString },
         { "{{SavedSearchName}}", _savedSearch.Name },
         { "{{IsTopLevel}}", IsTopLevelChecked },
+        { "{{EnteredSearchErrorMessage}}", _resources.GetResource("SaveSearchTemplateEnteredSearchError") },
+        { "{{EnteredSearchLabel}}", _resources.GetResource("SaveSearchTemplateEnteredSearchLabel") },
+        { "{{NameLabel}}", _resources.GetResource("SaveSearchTemplateNameLabel") },
+        { "{{NameErrorMessage}}", _resources.GetResource("SaveSearchTemplateNameError") },
+        { "{{IsTopLevelTitle}}", _resources.GetResource("SaveSearchTemplateIsTopLevelTitle") },
     };
 
     public SaveSearchForm(ISearchRepository searchRepository, IResources resources)
