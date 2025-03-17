9e7b6dd261fa0a26a75b95ec107af1ee14f31d84
Lauren Ciha <laurenciha@microsoft.com>
2025-03-17 19:56:10 -0400

    Add method to swap : for /

---
+++ GitHubExtension.Test/HelpersTests/StringExtensionsTest.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using GitHubExtension.Helpers;
+
+namespace GitHubExtension.Test.HelpersTests;
+
+[TestClass]
+public class StringExtensionsTest
+{
+    [TestMethod]
+    public void SwapGitColonsForForwardSlashes_ReplacesColonsWithForwardSlashes()
+    {
+        var input = "refs/heads/feature:branch";
+        var expected = "refs/heads/feature/branch";
+
+        var result = StringExtensions.SwapGitColonsForForwardSlashes(input);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void SwapGitColonsForForwardSlashes_NoColons_ReturnsSameString()
+    {
+        var input = "refs/heads/feature-branch";
+        var expected = "refs/heads/feature-branch";
+
+        var result = StringExtensions.SwapGitColonsForForwardSlashes(input);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void SwapGitColonsForForwardSlashes_EmptyString_ReturnsEmptyString()
+    {
+        var input = string.Empty;
+        var expected = string.Empty;
+
+        var result = StringExtensions.SwapGitColonsForForwardSlashes(input);
+
+        Assert.AreEqual(expected, result);
+    }
+}
--- GitHubExtension/Helpers/StringExtensions.cs
+++ GitHubExtension/Helpers/StringExtensions.cs
@@ -11,4 +11,20 @@ public static class StringExtensions
     public static string ToStringInvariant<T>(this T value) => Convert.ToString(value, CultureInfo.InvariantCulture)!;
 
     public static string FormatInvariant(this string value, params object[] arguments) => string.Format(CultureInfo.InvariantCulture, value, arguments);
+
+    public static string SwapGitColonsForForwardSlashes(string gitBranch)
+    {
+        if (gitBranch == null)
+        {
+            return string.Empty;
+        }
+
+        var parts = gitBranch.Split(':');
+        if (parts.Length > 1)
+        {
+            return string.Join("/", parts);
+        }
+
+        return gitBranch;
+    }
 }
