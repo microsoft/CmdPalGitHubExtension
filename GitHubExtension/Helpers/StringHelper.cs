e7cd6af989d4bba8edb0e652e5f3f7bd808d07b5
Lauren Ciha <laurenciha@microsoft.com>
2025-06-26 14:01:24 -0700

    Remove unused usings in StringHelper

--- GitHubExtension/Helpers/StringHelper.cs
+++ GitHubExtension/Helpers/StringHelper.cs
@@ -3,8 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Text.Json;
-using GitHubExtension.Client;
-using GitHubExtension.PersistentData;
 
 namespace GitHubExtension.Helpers;
 
