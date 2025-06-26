e2acc56be4a1e2d41143129644cd25faf401ce32
Lauren Ciha <laurenciha@microsoft.com>
2025-06-26 11:39:15 -0700

    Remove owner from pull request source branch for faster git checkout

--- GitHubExtension/Helpers/StringHelper.cs
+++ GitHubExtension/Helpers/StringHelper.cs
@@ -3,6 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Text.Json;
+using GitHubExtension.Client;
+using GitHubExtension.PersistentData;
 
 namespace GitHubExtension.Helpers;
 
